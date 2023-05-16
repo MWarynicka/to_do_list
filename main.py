e9b31c0aae737abbc0f65924598d375d23220d8b
MWarynicka <mwarynicka@gmail.com>
2023-05-16 19:24:47 +0200

    add base

--- main.py
+++ main.py
@@ -1,8 +1,21 @@
 tasks = {}
+def to_do():
+    x = input("What is the task? ")
+    y = input("What is the deadline? ")
+    tasks[x] = y
+    print(tasks)
 
-x= input("What is the task? ")
-y= input("What is the deadline? ")
-tasks[x]= y
+to_do()
+def to_do2():
+    z = input("Do you have another tasks ? yes or no ")
+    if z == "yes" :
+        to_do()
+        to_do2()
+    else:
+        print("That's all for now")
 
-print(tasks)
-z= input("Do you have another tasks ? yes or no ")
+to_do2()
+
+
+
+
