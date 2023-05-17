10d5eb93e0f0e27e51b4754d29b41702ab6af58c
MWarynicka <mwarynicka@gmail.com>
2023-05-17 20:34:32 +0200

    list

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 tasks = {}
-def table_of_tasks(tasks2):
-    tasks2={}
-    tasks= tasks2
-    for key, value in tasks2.items():
+
+
+def list():
+    for key, value in tasks.items():
         print(f'{key: <10}{value}')
-table_of_tasks()
+
 
 def to_do():
     x = input("What is the task? ")
@@ -13,30 +13,26 @@ def to_do():
 
 
 to_do()
+
+
 def to_do2():
     z = input("Do you have another tasks ? yes or no ")
-    if z == "yes" :
+    if z == "yes":
         to_do()
         to_do2()
     else:
         print("That's all for now")
 
+
 to_do2()
 
-def list_of_tasks():
-    l= input("Do you want to see all tasks? Yes or No ")
-    if l == "Yes":
-        table_of_tasks()
 
+def list_of_tasks():
+    l = input("Do you want to see all tasks? yes or no ")
+    if l == "yes":
+        list()
     else:
-        print("Bye")
+        print("bye")
+
 
 list_of_tasks()
-
-
-
-
-
-
-
-
